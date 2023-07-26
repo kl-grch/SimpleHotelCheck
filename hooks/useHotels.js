8264ebd91eb7641eecfeb74b581c63cc98b8ac1f
Nikola Gerasimov <kolyagerich@gmail.com>
2023-07-26 17:40:49 +0300

    'fallbackData'

--- hooks/useHotels.js
+++ hooks/useHotels.js
@@ -4,16 +4,23 @@ import useSWR from "swr";
 import { useDispatch } from "react-redux";
 import { getAllHotels } from "../components/foundHotels/foundHotelsSlise";
 
+const prefetchedData = [];
+
 export default function useHotels(location, checkIn, checkOut) {
   const dispatch = useDispatch();
   const url = `http://engine.hotellook.com/api/v2/cache.json?location=${location}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&limit=10`;
 
-  const fetcher = () =>
+  const fetcher = (url) =>
     fetch(url)
       .then((res) => res.json())
       .then((data) => dispatch(getAllHotels(data)));
 
-  const { data, error, isLoading } = useSWR(url, fetcher, { suspense: true });
+  const { data, error, isLoading } = useSWR(
+    url,
+    fetcher,
+    // { suspense: true },
+    { fallbackData: prefetchedData }
+  );
 
   return {
     data,
