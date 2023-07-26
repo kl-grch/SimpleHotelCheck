04d9636763218d83e3098941ec5ca1f413bebafc
Nikola Gerasimov <kolyagerich@gmail.com>
2023-07-26 17:56:55 +0300

    'update'

--- hooks/useHotels.js
+++ hooks/useHotels.js
@@ -4,8 +4,6 @@ import useSWR from "swr";
 import { useDispatch } from "react-redux";
 import { getAllHotels } from "../components/foundHotels/foundHotelsSlise";
 
-const prefetchedData = [];
-
 export default function useHotels(location, checkIn, checkOut) {
   const dispatch = useDispatch();
   const url = `http://engine.hotellook.com/api/v2/cache.json?location=${location}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&limit=10`;
@@ -19,7 +17,19 @@ export default function useHotels(location, checkIn, checkOut) {
     url,
     fetcher,
     // { suspense: true },
-    { fallbackData: prefetchedData }
+    {
+      fallbackData: [
+        {
+          hotelName: "Name",
+          stars: 5,
+          priceFrom: 1000,
+          countDays: 1,
+          checkIn: "2023-12-12",
+          hotelId: 1,
+          favoriteStatus: true,
+        },
+      ],
+    }
   );
 
   return {
